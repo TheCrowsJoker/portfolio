cb647385b5ed14de141cada11c2e165a6b516ae8
LiamSharpe <liamsmail1234@gmail.com>
2015-02-08 17:20:19 +1300

    The picture enlarges when clicked, there is a google form on the contact page, the border of the picture is fixed

--- flaskr.py
+++ flaskr.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/contact")
+def contactMe():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
