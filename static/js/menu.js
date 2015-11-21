d5a0ce1165e07256ec1228c9dc285ea5e1374722
TheCrowsJoker <liamsmail1234@gmail.com>
2015-11-21 20:04:32 +1300

    Fixed mobile menu breaking desktop menu problem

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,6 +1,11 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+
 $(document).ready(function() {
-	$(".menu").hide() //Hide the menu by default
-	$(".menu-button").click(function(){
-        $(".menu").slideToggle("fast");
-	});
+	if(isMobile) {
+		console.log("mobile");
+		$(".menu").hide() //Hide the menu by default
+		$(".menu-button").click(function(){
+	        $(".menu").slideToggle("fast");
+		});
+	};
 });
