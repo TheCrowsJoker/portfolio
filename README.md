3b9590dcef3d83bc1b168c244467c57416264861
Liam Sharpe <liamsmail1234@gmail.com>
2016-10-08 15:56:10 +1300

    Test push to temp repo

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ## PersonalWebsite
-This is my personal website that I created during February of 2015.
-It runs using a flask microframework on python and also uses HTML, CSS, and some javascript.
+This is the latest update to my personal website designed in July 2016
---
+++ static/js/mobile.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.menu-button').on('click',function() {
+        $(".nav").slideToggle("fast");
+    });
+    $(window).resize(function() {
+        if ($(window).width() > 750) {
+            $(".nav").show();
+        } else {
+            $(".nav").hide();
+        }
+    });
+});
