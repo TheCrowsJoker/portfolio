86bb036374f75643c2f1cb4a31f43d4ed0684717
Liam Sharpe <liam.sharpe@catalyst.net.nz>
2022-04-27 14:10:38 +1200

    Updated the gallery images and added in keybindings, updated the nav styling

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {
   faTimes,
   faChevronLeft,
@@ -16,14 +16,32 @@ export class GalleryComponent implements OnInit {
   faChevronRight = faChevronRight;
 
   images = [
+    'assets/images/gallery/rawcircus_10420212531_14458.jpg',
+    'assets/images/gallery/rawcircus_10420212531_20199.jpg',
+    'assets/images/gallery/rawcircus_10420212531_20211.jpg',
     'assets/images/gallery/rawcircus_10420212531_20265.jpg',
-    'assets/images/gallery/rawcircus_10420212531_22579.jpg',
+    'assets/images/gallery/rawcircus_10420212531_22538.jpg',
+    'assets/images/gallery/rawcircus_10420212531_22577.jpg',
+    'assets/images/gallery/rawcircus_10420212531_22632.jpg',
     'assets/images/gallery/rawcircus_10420212531_22723.jpg',
     'assets/images/gallery/rawcircusiceage_31720212540_8556.jpg',
     'assets/images/gallery/rawcircusiceage_31720212540_8716.jpg',
+    'assets/images/gallery/rawcircusiceage_31720212540_8747.jpg',
   ];
 
   fullImage: string | null = null;
+
+  /** Close modal on escape */
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      this.fullImage = null;
+    } else if (event.key === 'ArrowLeft') {
+      this.goToImage(-1);
+    } else if (event.key === 'ArrowRight') {
+      this.goToImage(1);
+    }
+  }
 
   constructor() {}
 
@@ -31,6 +49,7 @@ export class GalleryComponent implements OnInit {
 
   goToImage(index: number) {
     const currentImage = this.images.findIndex((i) => i === this.fullImage);
+
     if (currentImage === this.images.length - 1 && index === 1) {
       this.fullImage = this.images[0];
     } else if (currentImage === 0 && index === -1) {
